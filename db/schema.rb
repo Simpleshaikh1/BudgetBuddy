daf6d955a202101da0271a5a263d74af5a0d5265
Toyyib <hamzatbolaji648@yahoo.com>
2023-11-23 23:52:07 +0100

    added authorization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_23_223157) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_223710) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_223157) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "amount"
+    t.integer "author_id"
   end
 
   create_table "expenses_categories", force: :cascade do |t|
