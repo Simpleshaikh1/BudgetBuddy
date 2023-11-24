ad440de4f0c06fde715edcd9ac64f8616a688005
Toyyib <hamzatbolaji648@yahoo.com>
2023-11-24 03:41:47 +0100

    linters

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,21 +1,21 @@
 class ExpensesController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @group = Group.find(params[:group_id])
     @expenses = @group.expenses.order(created_at: :desc)
   end
-  
+
   def new
     @group = Group.find(params[:group_id])
     @expense = @group.expenses.new
   end
-  
+
   def create
     @group = Group.find(params[:group_id])
     @expense = @group.expenses.new(expense_params)
     @expense.author = current_user
-  
+
     if @expense.save
       @group.expenses << @expense
       redirect_to group_expenses_path(@group), notice: 'Transaction was successfully created.'
@@ -24,11 +24,10 @@ class ExpensesController < ApplicationController
       render :new
     end
   end
-  
+
   private
-  
+
   def expense_params
     params.require(:expense).permit(:name, :amount)
   end
 end
-  
